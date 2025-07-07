cee51159e1251579c9a5248070e6b7ae0ea6e2e5
Karlo Cehulic <karlocehlic@gmail.com>
2025-07-07 23:47:12 +0200

    Initial commit

---
+++ src/main/java/com/fer/connect/FerConnectApplication.java
@@ -0,0 +1,13 @@
+package com.fer.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerConnectApplication.class, args);
+	}
+
+}
