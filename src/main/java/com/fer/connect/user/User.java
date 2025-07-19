e30ba1580227c3bba6ed1f87a74f840c236c974d
Karlo Cehulic <karlocehlic@gmail.com>
2025-07-19 23:52:20 +0200

    Add basic user POST endpoint

---
+++ src/main/java/com/fer/connect/user/User.java
@@ -0,0 +1,71 @@
+package com.fer.connect.user;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "app_user")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column
+  private String firstName;
+
+  @Column
+  private String lastName;
+
+  @Column
+  private String email;
+
+  @Column
+  private String password;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
---
+++ src/main/java/com/fer/connect/user/UserController.java
@@ -0,0 +1,24 @@
+package com.fer.connect.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+
+  }
+
+  @PostMapping("/users")
+  public ResponseEntity<Object> postUser(@RequestBody User newUser) {
+    userService.save(newUser);
+
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+}
