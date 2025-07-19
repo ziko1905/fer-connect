aae2e5ad81eb7e15f4c6842a98a594c43f83ea94
Karlo Cehulic <karlocehlic@gmail.com>
2025-07-19 23:52:20 +0200

    Add basic user POST endpoint

--- src/test/java/com/fer/connect/FerConnectApplicationTests.java
+++ src/test/java/com/fer/connect/FerConnectApplicationTests.java
@@ -1,13 +1,15 @@
 package com.fer.connect;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FerConnectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void itRespondesWith200() {
+    assertEquals(true, true, "This should pass!");
+  }
 
 }
---
+++ src/test/java/com/fer/connect/user/UserControllerTest.java
@@ -0,0 +1,34 @@
+package com.fer.connect.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void itRespondesWithOkOnRightRequest() throws Exception {
+    String validUserJson = """
+        {
+          "firstName": "Karlo",
+          "lastName": "Čehulić",
+          "email": "randomemail@gmail.com",
+          "password": "Strong1",
+        }
+        """;
+
+    mockMvc
+        .perform(MockMvcRequestBuilders.post("/users").contentType(MediaType.APPLICATION_JSON).content(validUserJson))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+}
