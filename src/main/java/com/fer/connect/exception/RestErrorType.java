d1e7c8b778af9697b17f00658675af5d1ea5acda
Karlo Cehulic <karlocehlic@gmail.com>
2025-07-28 23:26:00 +0200

    Add Rest Error Types Enums

---
+++ src/main/java/com/fer/connect/exception/RestErrorType.java
@@ -0,0 +1,32 @@
+package com.fer.connect.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum RestErrorType {
+  USER_EMAIL_EXISTS(
+      "A user with that email already exists",
+      "email_already_exists",
+      HttpStatus.BAD_REQUEST);
+
+  private final String message;
+  private final String errorCode;
+  private final HttpStatus httpStatus;
+
+  RestErrorType(String defaultMessage, String defaultErrorCode, HttpStatus defaultHttpStatus) {
+    this.message = defaultMessage;
+    this.errorCode = defaultErrorCode;
+    this.httpStatus = defaultHttpStatus;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public String getErrorCode() {
+    return errorCode;
+  }
+
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
+  }
+}
