08aa7880ab205805968b27b094189fa1769f54fa
Karlo Cehulic <karlocehlic@gmail.com>
2025-07-31 01:59:33 +0200

    wip: unintended error handler

--- src/main/java/com/fer/connect/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fer/connect/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.fer.connect.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,12 +12,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(RestException.class)
-  public ResponseEntity<?> handleExceptions(RestException restException, ServletWebRequest request)
+  public ResponseEntity<?> handleIntendedExceptions(RestException restException, ServletWebRequest request)
       throws JsonProcessingException {
 
     String path = request.getRequest().getRequestURI();
     IntendedErrorResponse responseJson = new IntendedErrorResponse(restException, path);
 
     return ResponseEntity.status(restException.getHttpStatusCode()).body(responseJson);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<?> handleUnintendedExceptions(
+      Exception exception, ServletWebRequest request) {
+    String path = request.getRequest().getRequestURI();
+    UnintendedErrorResponse responseJson = new UnintendedErrorResponse(path, 400);
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseJson);
   }
 }
